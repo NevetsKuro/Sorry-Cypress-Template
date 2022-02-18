e553edfb55ca8489cc259d7e01035e6ff4d74bbe
Steven Fernandes <steven.fernandes@dripcapital.com>
2022-02-18 17:33:32 +0530

    chore: text changed

--- src/components/ClickToLazyLoad.spec.ts
+++ src/components/ClickToLazyLoad.spec.ts
@@ -2,7 +2,7 @@ import { mount } from '@cypress/vue'
 import ClickToLazyLoad from '@/components/ClickToLazyLoad.vue'
 
 describe('ClickToLazyLoad', () => {
-  it('loads a component lazily when clicking a button', () => {
+  it('Case: loads a component lazily when clicking a button', () => {
     mount(ClickToLazyLoad)
     cy.get('button').click()
     cy.get('p').should('have.text', `This is a test component to be loaded lazily with import('...')`)
