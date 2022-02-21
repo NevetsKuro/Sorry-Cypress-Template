b4fe664fac7d91324a14d2c623c4630bd4cc5de5
Steven Fernandes <steven.fernandes@dripcapital.com>
2022-02-21 15:36:49 +0530

    fix: title

--- cypress/integration/sampleTest.spec.js
+++ cypress/integration/sampleTest.spec.js
@@ -8,7 +8,7 @@ describe(pages.homePage, () => {
     expect(true).to.equal(true);
   });
   it('Check for title', () => {
-    cy.contains('Welcome to Your Vue.js + TypeScript App');
+    cy.contains('Connecting Trade with Technology');
   });
   // it('Does image render', () => {
   //   cy.fixture('images/who-we-are.jpg', null).then((logo) => {
