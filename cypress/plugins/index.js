6cc91e60c6c60d5cd660a39c902990445d15ddf0
Steven Fernandes <steven.fernandes@dripcapital.com>
2022-02-21 15:06:31 +0530

    chore: add mocha reporter

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,6 +2,7 @@ const { startDevServer } = require('@cypress/webpack-dev-server')
 const webpackConfig = require('@vue/cli-service/webpack.config.js')
 
 module.exports = (on, config) => {
+  require('cypress-mochawesome-reporter/plugin')(on);
   on('dev-server:start', options =>
     startDevServer({
       options,
